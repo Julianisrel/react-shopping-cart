71ce6dd5b045454c4342d082fb05ab58a8cc8c64
Julian Cole <juliancole@Julians-MacBook-Pro.local>
2020-07-11 10:55:09 -0700

    step 3

--- src/App.js
+++ src/App.js
@@ -2,10 +2,13 @@ import React, { useState } from 'react';
 import { Route } from 'react-router-dom';
 import data from './data';
 
+
+
 // Components
 import Navigation from './components/Navigation';
 import Products from './components/Products';
 import ShoppingCart from './components/ShoppingCart';
+import ProductContext from './contexts/ProductContext';
 
 function App() {
 	const [products] = useState(data);
@@ -27,6 +30,7 @@ function App() {
 
 	return (
 		<div className="App">
+			<ProductContext.Provider value={{products, addItem}}>
 			<Navigation cart={cart} />
 
 			{/* Routes */}
@@ -37,6 +41,7 @@ function App() {
 			<Route path="/cart">
 				<ShoppingCart cart={cart} />
 			</Route>
+			</ProductContext.Provider>
 		</div>
 	);
 }
